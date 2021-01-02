e47b816b07cdf260b7dfa30a6ee18a9dab430aef
Phyo Thiha <phyothiha96@gmail.com>
2021-01-03 00:58:21 +0630

    Add auth function and User Class for Auth Class Dependency

--- auth.php
+++ auth.php
@@ -1,6 +1,26 @@
 <?php
 
+class User
+{
+    protected $name;
+    protected $password;
+
+    public function __construct($name, $password)
+    {
+	$this->name = $name;
+	$this->password = $password;
+    }
+
+}
+
+
 class Auth
 {
-    // Implementation Auth Service
+    public function auth()
+    {
+	return $this->user;
+    }
 }
+
+$user = new Auth(new User('Zaw Zaw', 'encrypted_password'));
+$user->auth();	
